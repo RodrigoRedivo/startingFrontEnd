229b8d73e0d6ea7d4d168fdf94ca54b0a833d358
RodrigoRedivo <rodrigo.a.redivo@gmail.com>
2020-02-02 00:29:41 -0300

    update FrontEnd

---
+++ Aula/scripts.js
@@ -0,0 +1,32 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards) {
+  card.addEventListener('click', function(){
+    const videoId = card.getAttribute('id')
+    modalOverlay.classList.add('active')
+    modalOverlay.querySelector('iframe').src = `https://www.youtube.com/embed/${videoId}`
+  })
+}
+
+document.querySelector('.close-modal').addEventListener('click', function(){
+  modalOverlay.classList.remove('active')
+  modalOverlay.querySelector("iframe").src = '' 
+})
+
+modalOverlay.addEventListener('click', function(e) {
+  if (e.target == this) fecha();
+});
+
+function abre() {
+  modal.style.display = 'block';
+}
+
+function fecha() {
+  modal.style.display = 'none';
+}
+
+
+
+
+
