7baa14795e779677c5f4e6560492d41187bf885b
RodrigoRedivo <rodrigo.a.redivo@gmail.com>
2020-02-02 22:22:28 -0300

    Create FrontEnd desafio3

---
+++ Desafio3/scripts.js
@@ -0,0 +1,19 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards) {
+  card.addEventListener('click', function(){
+    const imgId = card.getAttribute('id')
+    modalOverlay.classList.add('active')
+    modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${imgId}`
+  })
+}
+
+document.querySelector('.close-modal').addEventListener('click', function(){
+  modalOverlay.classList.remove('active')
+  modalOverlay.querySelector("iframe").src = '' 
+})
+
+document.querySelector('.maximize-modal').addEventListener('click', function(){
+  modalOverlay.classList.add('active')
+})
