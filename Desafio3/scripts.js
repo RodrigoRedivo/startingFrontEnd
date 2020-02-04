b6721fcbf3d0975ff9caa6488b6cd2d92fce03ee
RodrigoRedivo <rodrigo.a.redivo@gmail.com>
2020-02-03 22:57:53 -0300

    Update FrontEnd desafio3

--- Desafio3/scripts.js
+++ Desafio3/scripts.js
@@ -1,19 +1,40 @@
-const modalOverlay = document.querySelector('.modal-overlay')
+const modalOverlay = document.querySelector('.modal_overlay')
 const cards = document.querySelectorAll('.card')
+const modal = document.querySelector('.modal')
 
-for (let card of cards) {
-  card.addEventListener('click', function(){
-    const imgId = card.getAttribute('id')
-    modalOverlay.classList.add('active')
-    modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${imgId}`
-  })
+let cont = 0
+for(let card of cards){
+    const id_card = card.getAttribute("id")
+    card.addEventListener("click", function(){
+       modalOverlay.classList.add('active')
+       modalOverlay.querySelector("iframe").src=`https://rocketseat.com.br/${id_card}`
+
+    })
 }
 
-document.querySelector('.close-modal').addEventListener('click', function(){
-  modalOverlay.classList.remove('active')
-  modalOverlay.querySelector("iframe").src = '' 
+// Fechando pelo X
+document.querySelector('.close_modal').addEventListener('click',function(){
+    modalOverlay.classList.remove('active')
+    modalOverlay.querySelector("iframe").src=''
 })
 
-document.querySelector('.maximize-modal').addEventListener('click', function(){
-  modalOverlay.classList.add('active')
+// Fechando atraves de um click fora da janela aberta
+modalOverlay.addEventListener('click', function(event) {
+    if(event.target === modalOverlay) {
+    modalOverlay.classList.remove('active')
+    modalOverlay.querySelector("iframe").src=''
+    }
+})
+
+// Maximizando
+document.querySelector('.maximize_modal').addEventListener('click', function(){
+    
+  if(cont==0){
+      modal.classList.add('fullscreen')
+      cont++
+  }
+  else{
+      modal.classList.remove('fullscreen')
+      cont = 0
+  }  
 })
